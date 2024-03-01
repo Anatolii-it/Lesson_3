15bc775638768f9ba146831dfe8869e85d773d4d
Anatolii-it <t0663261690@gmail.com>
2024-03-01 16:58:00 +0200

    2

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+using namespace std;
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+
+        const int SIZE = 5;
+        int array[SIZE] = { 1, 2, 3, 4, 5 };
+
+        // Початковий вивід масиву
+        cout << "Початковий масив:" << endl;
+        for (int i = 0; i < SIZE; ++i) {
+            cout << array[i] << " ";
+        }
+        cout << endl;
+
+        // Покажчик на перший елемент масиву
+        int* ptr = array;
+
+        // Покажчик на останній елемент масиву
+        int* endPtr = array + SIZE - 1;
+
+        // Проходимося по масиву з обох кінців і замінюємо елементи
+        while (ptr < endPtr) {
+            
+            int temp = *ptr;
+            *ptr = *endPtr;
+            *endPtr = temp;
+
+            
+            ++ptr;
+            --endPtr;
+        }
+
+        
+        cout << "Масив зі зміненим порядком:" << endl;
+        for (int i = 0; i < SIZE; ++i) {
+            cout << array[i] << " ";
+        }
+        cout << endl;
+
+        return 0;
+    
+
+
+
+	_getch();
+	return 0;
+}
