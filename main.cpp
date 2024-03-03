90e2950bed4b7bbf3009ae40514531073f9c36da
Anatolii-it <t0663261690@gmail.com>
2024-03-03 10:27:12 +0200

    task2

--- main.cpp
+++ main.cpp
@@ -7,46 +7,29 @@ using namespace std;
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	
-
         const int SIZE = 5;
         int array[SIZE] = { 1, 2, 3, 4, 5 };
-
-        
         cout << "Початковий масив:" << endl;
         for (int i = 0; i < SIZE; ++i) {
             cout << array[i] << " ";
         }
         cout << endl;
-
-        
         int* ptr = array;
-
-       
         int* endPtr = array + SIZE - 1;
-
-        
         while (ptr < endPtr) {
             
             int temp = *ptr;
             *ptr = *endPtr;
             *endPtr = temp;
-
-            
             ++ptr;
             --endPtr;
         }
 
-        
         cout << "Масив зі зміненим порядком:" << endl;
         for (int i = 0; i < SIZE; ++i) {
             cout << array[i] << " ";
         }
         cout << endl;
-
-       
-    
-
 
 
 	_getch();
